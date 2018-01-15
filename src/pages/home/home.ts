b5de67c1443eaf06449f29fbb77720a4d65f0bc2
Josep Roman Martinez Riera <pepinho@gmail.com>
2018-01-15 14:49:50 +0100

    takephoto

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 
@@ -9,42 +9,74 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 export class HomePage {
   public base64Image: string;
   public chose64Image: string;
+  public url: string;
+  checked: boolean = false;
 
-  constructor(public navCtrl: NavController, private camera: Camera) { }
+  constructor(
+    public navCtrl: NavController,
+    private camera: Camera,
+    private element: ElementRef
+  ) {}
 
   takePicture() {
     const options: CameraOptions = {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    }
+      sourceType: this.camera.PictureSourceType.CAMERA
+    };
 
-    this.camera.getPicture(options).then((imageData) => {
-      // imageData is either a base64 encoded string or a file URI
-      // If it's base64:
-      this.base64Image = 'data:image/jpeg;base64,' + imageData;
-    }, (err) => {
-      // Handle error
-    });
+    this.camera
+      .getPicture({
+        quality: 100,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        sourceType: this.camera.PictureSourceType.CAMERA
+      })
+      .then(
+        imageData => {
+          // imageData is either a base64 encoded string or a file URI
+          // If it's base64:
+          console.log('Image Base 64' + imageData);
+          this.base64Image = 'data:image/jpeg;base64,' + imageData;
+        },
+        err => {
+          // Handle error
+        }
+      );
   }
 
   choosePicture() {
     const options: CameraOptions = {
       quality: 100,
       sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    }
+      //destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG
+    };
 
-    this.camera.getPicture(options).then((imageData) => {
-      // imageData is either a base64 encoded string or a file URI
-      // If it's base64:
-      this.chose64Image = 'data:image/jpeg;base64,' + imageData;
-    }, (err) => {
-      // Handle error
-    });
+    this.camera.getPicture(options).then(
+      file_uri => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64:
+
+        this.chose64Image = 'data:image/jpeg;base64,' + file_uri;
+        console.log('Image URI' + file_uri);
+      },
+      err => {
+        // Handle error
+      }
+    );
+  }
+
+  readUrl(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = (event: any) => {
+        this.url = event.target.result;
+      };
+
+      reader.readAsDataURL(event.target.files[0]);
+    }
   }
 }
-
